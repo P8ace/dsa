e2914228bd11d6339ba70ec3d051c7f61087dc99
Salman Shaik <salmansiddiq.shaik@gmail.com>
2026-04-06 06:10:02 +0000

    subarray sum equal k with prefix sum in go

---
+++ go/practise150/a2_sub_arrays_ksum.go
@@ -0,0 +1,23 @@
+package practise150
+
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+func NoofSubarraysofsumK(nums []int, k int) int {
+	// Use prefix sum with hashmap when you need to match a sum of k and count sub arrays
+	prefixSum := 0
+	count := 0
+	sumMap := map[int]int{
+		0: 1,
+	}
+
+	for i:= 0; i < len(nums); i++ {
+		prefixSum += nums[i]
+		diff := prefixSum - k
+		if val, ok := sumMap[diff]; ok {
+			count += val
+		}
+		sumMap[prefixSum]++
+	}
+	return count
+}
---
+++ go/practise150/a2_sub_arrays_ksum_test.go
@@ -0,0 +1,58 @@
+package practise150
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNoofSubarrays(t *testing.T) {
+	test_cases := []struct {
+		input    []int
+		k        int
+		expected int
+	}{
+		{
+			input:    []int{1, 1, 1},
+			k:        2,
+			expected: 2,
+		},
+		{
+			input:    []int{1, 3, 5},
+			k:        0,
+			expected: 0,
+		},
+		{
+			input:    []int{1, 2, 3},
+			k:        3,
+			expected: 2,
+		},
+		{
+			input:    []int{4, 4, 4, 4, 4, 4},
+			k:        4,
+			expected: 6,
+		},
+		{
+			input:    []int{1, 2, 1},
+			k:        5,
+			expected: 0,
+		},
+		{
+			input:    []int{10, 2, -2, -20, 10},
+			k:        -10,
+			expected: 3,
+		},
+		{
+			input:    []int{9, 4, 20, 3, 10, 5},
+			k:        33,
+			expected: 2,
+		},
+	}
+	for idx, test_case := range test_cases {
+		t.Run(fmt.Sprintf("test_case_no:%d", idx+1), func(t *testing.T) {
+			actual := NoofSubarraysofsumK(test_case.input, test_case.k)
+			if actual != test_case.expected {
+				t.Errorf("Testing no of sub array of sum K. Input: %v, K: %d, Expected: %d, Actual: %d.", test_case.input, test_case.k, test_case.expected, actual)
+			}
+		})
+	}
+}
